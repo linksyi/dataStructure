809d8c5419a5aac278ae7e81f55cefc557f3e1e7
Mr.Z <1040432374@qq.com>
2019-08-07 23:15:21 +0800

    递归实现迷宫中寻找某个点

--- src/structure/recursion/Main.java
+++ src/structure/recursion/Main.java
@@ -1,8 +1,16 @@
 package structure.recursion;
 
+import structure.recursion.demo.Maze;
+
 /**
  * @author My.ZuoY
  * @date 2019-08-05 22:04
  */
-public class main {
+public class Main {
+
+    public static void main(String[] args) {
+        Maze maze = new Maze();
+        maze.walk(1, 1);
+        maze.print();
+    }
 }
--- src/structure/recursion/demo/Maze.java
+++ src/structure/recursion/demo/Maze.java
@@ -1,4 +1,4 @@
-package structure.recursion;
+package structure.recursion.demo;
 
 /**
  * @author My.ZuoY
@@ -7,7 +7,8 @@ package structure.recursion;
  */
 public class Maze {
     private int[][] mazeArr = new int[8][8];
-    public Maze(){
+
+    public Maze() {
         maze();
     }
 
@@ -15,20 +16,23 @@ public class Maze {
      * 数组元素为1表示墙，数组元素为2表示行走路线，数组元素为-1表示已行走走不通，数组元素为0表示还没行走的方格
      * 怎么定义迷宫的墙可以自己定义
      */
-    private void maze(){
-        for(int i = 0; i < mazeArr.length; i++){
+    private void maze() {
+        for (int i = 0; i < mazeArr.length; i++) {
             mazeArr[0][i] = 1;
             mazeArr[7][i] = 1;
         }
-        for (int i = 1; i < mazeArr.length - 1; i++){
+        for (int i = 1; i < mazeArr.length - 1; i++) {
             mazeArr[i][0] = 1;
             mazeArr[i][7] = 1;
         }
+        mazeArr[6][2] = 1;
+        mazeArr[4][3] = 1;
+        mazeArr[4][5] = 1;
     }
 
-    public void test(){
+    public void print() {
         for (int i = 0; i < mazeArr.length; i++) {
-            for (int j = 0; j < mazeArr[i].length ; j++) {
+            for (int j = 0; j < mazeArr[i].length; j++) {
                 System.out.print(mazeArr[i][j] + "\t");
             }
             System.out.println();
@@ -36,29 +40,36 @@ public class Maze {
     }
 
     /**
+     * 移动目前所在位置
      *
      * @param i 当前纵坐标
      * @param j 当前横坐标
      */
-    public void walk(int i,int j){
-        if(mazeArr[6][6] == 2){
+    public void walk(int i, int j) {
+        if (mazeArr[6][6] == 2) {
             return;
         }
         mazeArr[i][j] = 2;
-        if(judge(i-1,j)){
-            walk(i-1, j);
-        }else if(judge(i,j+1)){
-            walk(i,j+1);
-        }else if(judge(i+1,j)){
-            walk(i+1,j);
-        }else if(judge(i,j-1)){
-            walk(i,j-1);
-        }else{
-            mazeArr[i][j] = -1;
+        if (judge(i + 1, j)) {
+            walk(i + 1, j);
         }
+        if (judge(i, j + 1)) {
+            walk(i, j + 1);
+        }
+        if (judge(i - 1, j)) {
+            walk(i - 1, j);
+        }
+        if (judge(i, j - 1)) {
+            walk(i, j - 1);
+        }
+        if (mazeArr[6][6] == 2) {
+            return;
+        }
+        mazeArr[i][j] = -1;
+
     }
 
-    public boolean judge(int i,int j){
+    public boolean judge(int i, int j) {
         return mazeArr[i][j] == 0;
     }
 }
