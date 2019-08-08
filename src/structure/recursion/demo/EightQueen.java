03beea48c7c6e3c12d870d6a88a112f7902fffc0
Mr.Z <1040432374@qq.com>
2019-08-08 22:26:07 +0800

    递归解决八皇后问题

---
+++ src/structure/recursion/demo/EightQueen.java
@@ -0,0 +1,14 @@
+package structure.recursion.demo;
+
+/**
+ * @author My.ZuoY
+ * @date 2019-08-07 20:33
+ * 递归解决八皇后问题
+ */
+public class EightQueen {
+    private int[] result;
+
+    public boolean judge(){
+        return false;
+    }
+}
